b3b3262ef1bc9ec3643c5f55fa3d035d2e206ec3
EngerThen <engerthen4d17@gmail.com>
2018-11-09 01:18:41 -0500

    This is the login window for our app

---
+++ src/sample/LoginController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+
+public class LoginController {
+
+    @FXML
+    TextField userNameField;
+
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    Button login;
+
+    @FXML
+    Button signUp;
+
+    @FXML
+    Button guestAccess;
+}
