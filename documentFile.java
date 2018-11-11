b1770528e356bcdf9bf9de1c9f11ab90a4755a87
Steven Melamud <stevenm0429@gmail.com>
2018-11-10 23:51:12 -0500

    Add files via upload

---
+++ documentFile.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class documentFile {
+
+
+    private int iD = 0;
+    private String name;
+    private boolean lock = false;
+    private boolean restricted = false;
+    private String mainText = null;
+    private Date date;
+    private String userName;
+    private int securityLevel = 0;
+
+}
