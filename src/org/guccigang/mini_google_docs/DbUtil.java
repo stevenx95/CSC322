e1fe232b4433d43d44e0b4687c2d29123e72bcf7
EngerThen <engerthen4d17@gmail.com>
2018-11-18 00:59:23 -0500

    PlacedSteve's class inside the package

--- src/org/guccigang/mini_google_docs/DbUtil.java
+++ src/org/guccigang/mini_google_docs/DbUtil.java
@@ -8,10 +8,8 @@
         get results of query
  */
 package org.guccigang.mini_google_docs;
-//import com.sun.rowset.CachedRowSetImpl;
 
 import java.sql.*;
-import javax.swing.JOptionPane; // this class comes from the swing library it produces a pop up windows
 //I use it here instead of JavaFX becasue it is quicker to code and use.
 
 public class DbUtil {
@@ -53,45 +51,6 @@ public class DbUtil {
             e.printStackTrace();
         }
     }
-
-//    //Execute Query Operation
-//    public static ResultSet executQueryDB(String queryStatment) throws SQLException, ClassNotFoundException {
-//        Statement statement = null;
-//        ResultSet resultSet = null;
-//        //CachedRowSetImpl crs = null;
-//
-//        try {
-//            //Connect to the database
-//            connectDB();
-//            System.out.println("Select statment: " + queryStatment + "\n");
-//            //Create statment
-//            statement = connection.createStatement();
-//            //Execute select query statment.
-//            resultSet = statement.executeQuery(queryStatment);
-//
-//            /**
-//             * CachedRowSet , in order to prevent "java.sql.SQLRECOVERABLEEXCEPTION: CLOSED CONNECTION: NEXT" error
-//             * We use cachedrowset.
-//             */
-//            crs = new CachedRowSetImpl();
-//            crs.populate(resultSet);
-//
-//        } catch (SQLException e) {
-//            System.out.println("A problemhas occurred please check query statment." + e);
-//            throw e;
-//        } finally {
-//            if (resultSet != null) {
-//                //Close result set
-//                resultSet.close();
-//            }
-//            if (statement != null) {
-//                statement.close();
-//            }
-//            disconnectDB();
-//        }
-//        return crs;
-//
-//    }
 
     //Execute update for (insert,update,delete) operations
     public static int executeUpdateDB(String sqlStatement, String... sqlParams) throws SQLException  {
