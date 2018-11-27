e45ade06367694c7840bfb0e45f0b00d2fd1ad02
kieran duggan <kieranduggan15@gmail.com>
2018-11-26 22:56:13 -0500

    Updated SQL tables to account for interests 4 AND Added stubs for version util

--- sql database script/sql script document system.sql
+++ sql database script/sql script document system.sql
@@ -1,8 +1,9 @@
 drop table if exists revisions;
 drop table if exists sharedDocs;
+drop table if exists interests;
 drop table if exists documents;
 drop table if exists users;
-drop table if exists interests;
+
 
 CREATE TABLE application (
     userName varchar(20) PRIMARY KEY,
@@ -24,22 +25,28 @@ CREATE TABLE users (
 );
 
 CREATE TABLE interests (
+    userName varchar(20),
     FOREIGN KEY (userName) REFERENCES users(userName),
     interest varchar(20)
 );
 
 CREATE TABLE documents (
-    userName varchar(20) NOT NULL,
-    docName varchar(20), docID int,
-    version int,
+    docID int PRIMARY KEY,
+    owner varchar(20),
+    FOREIGN KEY (owner) REFERENCES users(userName),
+    docName varchar(20),
     content text,
     isLocked int NOT NULL,
-    resticted int NOT NULL,
+    restricted int NOT NULL,
     createdDate date,
-    counter int,
-    tabooFlag int,
-    PRIMARY KEY (docID, version),
-    FOREIGN KEY (userName) REFERENCES users(userName)
+    tabooFlag int
+);
+
+CREATE TABLE revisions (
+    docID int(50) NOT NULL,
+    version int NOT NULL,
+    dateOfEdit date NOT NULL,
+    FOREIGN KEY (docID) REFERENCES documents(docID)
 );
 
 CREATE TABLE sharedDocs (
@@ -50,19 +57,23 @@ CREATE TABLE sharedDocs (
 );
 /*Does sharedDoc table need the version number also? */
 
-CREATE TABLE revisions (
-    userName varchar(20) NOT NULL,
-    docID int(50) NOT NULL,
-    version int NOT NULL,
-    dateOfEdit date NOT NULL,
-    FOREIGN KEY (userName) REFERENCES users(userName),
-    FOREIGN KEY (docID, version) REFERENCES documents(docID, version)
-);
 
-insert into users value("Jon", "password", "Jonathan", "Tran", "working out", "sleeping", "eating", 1);
-insert into users value("Peter", "password", "Jonathan", "Tran", "working out", "sleeping", "eating", 1);
-insert into users value("Ant", "password", "Jonathan", "Tran", "working out", "sleeping", "eating", 1);
-insert into users value("Kelly", "password", "Jonathan", "Tran", "working out", "sleeping", "eating", 1);
+insert into users value("Jon", "password", "Jonathan", "Tran", 1);
+insert into interests value("Jon", "Sleeping");
+insert into interests value("Jon", "Eating");
+insert into interests value("Jon", "Lifting");
+insert into users value("Peter", "password", "Peter", "Phung", 1);
+insert into interests value("Peter", "Filming");
+insert into interests value("Peter", "Engineering");
+insert into interests value("Peter", "Photography");
+insert into users value("Ant", "password", "Ant", "Hony", 1);
+insert into interests value("Ant", "Youtubing");
+insert into interests value("Ant", "Gaming");
+insert into interests value("Ant", "Writing Sketches");
+insert into users value("Kelly", "password", "Kelly", "Tran", 1);
+insert into interests value("Kelly", "Sleeping");
+insert into interests value("Kelly", "Doing Nothing");
+insert into interests value("Kelly", "Lifting");
 
 insert into documents value("Jon","my shoping list",1111,1,"GEARS OF WAR, HALO 2, WAR Z",0,0,'2008-11-11',2);
 insert into documents value("Peter","gun control?",2222,1,"nah",0,0,'2008-11-11',2);
---
+++ src/org/guccigang/mini_google_docs/VersionUtil.java
@@ -0,0 +1,17 @@
+
+/*
+This is a utility class for version control
+ */
+package org.guccigang.mini_google_docs;
+import java.util.*;
+public class VersionUtil
+{
+    public static String getChanges(String str1, String str2)
+    {
+        return "stub";
+    }
+    public static String apply(String originalString, String listOfChanges)
+    {
+        return "stub";
+    }
+}
