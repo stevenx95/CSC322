e5d60ea122f90e208231a89f7720520e2e906118
EngerThen <engerthen4d17@gmail.com>
2018-11-26 19:15:02 -0500

    optimized DbUtil

--- src/org/guccigang/mini_google_docs/Main.java
+++ src/org/guccigang/mini_google_docs/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Mini Google Docs Login");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
         //for testing purposes.
