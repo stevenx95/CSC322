9e9aef8bf5fed11ea36023c246130de8652dea29
kieran duggan <kieranduggan15@gmail.com>
2018-11-30 22:33:38 -0500

    Modified debugging tools structure

---
+++ src/org/guccigang/mini_google_docs/DebugUtil.java
@@ -0,0 +1,9 @@
+package org.guccigang.mini_google_docs;
+
+public class DebugUtil
+{
+    public static void execCommand(String[] args)
+    {
+
+    }
+}
--- src/org/guccigang/mini_google_docs/Main.java
+++ src/org/guccigang/mini_google_docs/Main.java
@@ -1,14 +1,16 @@
 package org.guccigang.mini_google_docs;
 
+import java.util.Scanner;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
 import org.guccigang.mini_google_docs.model.DocumentFile;
+import org.guccigang.mini_google_docs.DebugUtil;
 
 public class Main extends Application {
     //List of documents for table view in view document controller.
@@ -17,13 +19,21 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
+        Scene scene = new Scene(root, 600, 400);
         primaryStage.setTitle("Mini Google Docs Login");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(scene);
         primaryStage.show();
 
+        //Debug escape character
+        scene.setOnKeyPressed(e ->
+        {
+            Scanner keyboard = new Scanner(System.in);
+            if (e.getCode() == KeyCode.BACK_QUOTE) {
+                System.out.println("Enter debug command:\n");
+                DebugUtil.execCommand(keyboard.nextLine().split(" "));
+            }
+        });
     }
-
-
 
 
     public static void main(String[] args) {
