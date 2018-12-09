511f34005dd475739e8cd62a1942debb37d2eddc
kieran duggan <kieranduggan15@gmail.com>
2018-12-09 18:54:43 -0500

    Started work on the text editor

--- src/org/guccigang/mini_google_docs/controller/DocumentControllers/SuperAndOriginalTextEditorController.java
+++ src/org/guccigang/mini_google_docs/controller/DocumentControllers/SuperAndOriginalTextEditorController.java
@@ -1,12 +1,16 @@
 package org.guccigang.mini_google_docs.controller.DocumentControllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
 import org.guccigang.mini_google_docs.model.DocumentFile;
 import org.guccigang.mini_google_docs.model.UserObject;
+import org.guccigang.mini_google_docs.model.VersionUtil;
 
 public class SuperAndOriginalTextEditorController {
 
     private UserObject currentUser;
     private DocumentFile selectedDocument;
+    private TextArea areaText;
 
     public SuperAndOriginalTextEditorController() {
         this(null, null);
@@ -18,4 +22,29 @@ public class SuperAndOriginalTextEditorController {
 
     }
 
+    public void onSave(ActionEvent event)
+    {
+        try{
+            VersionUtil.save(Integer.toString(selectedDocument.getID()),areaText.getText(),currentUser.getUserName());
+        }
+        catch (java.sql.SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void onLoad(ActionEvent event)
+    {
+        System.out.println("Stub! OnLoad");
+    }
+
+    public void onClose(ActionEvent event)
+    {
+        System.out.println("Stub! OnClose");
+    }
+
+    public void onAbout(ActionEvent event)
+    {
+        System.out.println("Stub! OnAbout");
+    }
 }
