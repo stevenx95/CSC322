d905f2c9ee1ac6b0cfb2a78636d96bea7cd565d2
stevenx95 <stevenm0429@gmail.com>
2018-12-11 16:57:01 -0500

    Implemented home button for SU complaint view

--- src/org/guccigang/mini_google_docs/controller/ComplaintControllers/OUUserComplaintViewController.java
+++ src/org/guccigang/mini_google_docs/controller/ComplaintControllers/OUUserComplaintViewController.java
@@ -1,13 +1,18 @@
 package org.guccigang.mini_google_docs.controller.ComplaintControllers;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import org.guccigang.mini_google_docs.UILocation;
+import org.guccigang.mini_google_docs.controller.UserUI.OriginalUserUIController;
+import org.guccigang.mini_google_docs.controller.UserUI.SuperUserUIController;
 import org.guccigang.mini_google_docs.model.*;
 import org.guccigang.mini_google_docs.model.UserComplaint;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -70,6 +75,23 @@ public class OUUserComplaintViewController {
         return;
     }
 
-    
+    @FXML
+    public void handleHome(ActionEvent event){
+        ReturnToProfile(event, currentUser);
+    }
+
+    public static void ReturnToProfile(ActionEvent event, UserObject currentUser) {
+        try{
+            if(currentUser.getMembershipLevel()==1){
+                OriginalUserUIController controller = new OriginalUserUIController(currentUser);
+                GuiUtil.changeScene(event, UILocation.ORIGINAL_USER_UI,"Home", controller);
+            }else {
+                SuperUserUIController controller = new SuperUserUIController(currentUser);
+                GuiUtil.changeScene(event,UILocation.SUPER_USER_UI,"Home", controller);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
     
 }
